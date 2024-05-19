871562fa53170ad4d7178032857b4449bfb142bc
cklinova <cklinova@productiveedge.com>
2024-05-19 13:59:00 +0200

    New tests in CreateNewProjectTest

--- src/main/java/com/example/teamcity/ui/Selectors.java
+++ src/main/java/com/example/teamcity/ui/Selectors.java
@@ -19,4 +19,8 @@ public class Selectors {
     public static ByAttribute byClass(String value){
         return new ByAttribute("class", value);
     }
+
+    public static ByAttribute byHref(String value){
+        return new ByAttribute("href", value);
+    }
 }
--- src/test/java/com/example/teamcity/ui/CreateNewProjectTest.java
+++ src/test/java/com/example/teamcity/ui/CreateNewProjectTest.java
@@ -1,6 +1,7 @@
 package com.example.teamcity.ui;
 
 import com.codeborne.selenide.Condition;
+import com.example.teamcity.api.generators.RandomData;
 import com.example.teamcity.ui.pages.admin.CreateNewProject;
 import com.example.teamcity.ui.pages.favorites.ProjectsPage;
 import org.testng.annotations.Test;
@@ -16,7 +17,7 @@ public class CreateNewProjectTest extends BaseUiTest {
         new CreateNewProject()
                 .open(testData.getProject().getParentProject().getLocator())
                 .createProjectByUrl(url)
-                .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
+                .setupProjectFromRepo(testData.getProject().getName(), testData.getBuildType().getName());
 
         new ProjectsPage().open()
                 .getSubprojects()
@@ -27,18 +28,46 @@ public class CreateNewProjectTest extends BaseUiTest {
     @Test
     public void authorizedUserShouldBeAbleToCreateNewProjectManually(){
         var testData = testDataStorage.addTestData();
-        var url = "https://github.com/AlexPshe/spring-core-for-qa";
 
         loginAsUser(testData.getUser());
 
         new CreateNewProject()
                 .open(testData.getProject().getParentProject().getLocator())
-                .createProjectByUrl(url)
-                .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
+                .setupProjectManually(testData.getProject().getName(), testData.getProject().getId());
 
         new ProjectsPage().open()
                 .getSubprojects()
                 .stream().reduce((first, second) -> second).get()
                 .getHeader().shouldHave(Condition.text(testData.getProject().getName()));
+    }
+
+    @Test
+    public void authorizedUserShouldNotBeAbleToCreateNewProjectFromPrivateRepository(){
+        var testData = testDataStorage.addTestData();
+        var url = "https://github.com/katerina-klinova/teamcity-testing-framework";
+        var createNewProjPage = new CreateNewProject();
+
+        loginAsUser(testData.getUser());
+
+        createNewProjPage.open(testData.getProject().getParentProject().getLocator())
+                .createProjectByUrl(url);
+        createNewProjPage.getUrlErrorMessage().shouldHave(
+                Condition.text("Anonymous authentication has failed. The repository is either private or does not exist"));
+    }
+
+    @Test
+    public void authorizedUserShouldNotBeAbleToCreateNewProjectManuallyWithExistingName(){
+        var testData = testDataStorage.addTestData();
+        var createNewProjPage = new CreateNewProject();
+        var project = testData.getProject();
+
+        loginAsUser(testData.getUser());
+
+        createNewProjPage.open(project.getParentProject().getLocator())
+                .setupProjectManually(project.getName(), project.getId());
+        createNewProjPage.open(project.getParentProject().getLocator())
+                .setupProjectManually(project.getName(), RandomData.getString());
+        createNewProjPage.getNameErrorMessage().shouldHave(
+                Condition.text("Project with this name already exists:"));
     }
 }
